5e605fa413d9d96d31914733068d3d1cedd4e2e4
Carlos Araki <carlosarak@gmail.com>
2017-08-15 13:08:22 -0300

    bug fixes

--- ADADOS/LISTA1.m
+++ ADADOS/LISTA1.m
@@ -32,6 +32,7 @@ fx =1+real(exp(i.*x))-cos(x);
 plot(x,fx)
 
 
+
 %ex 1.6
 k= -10;
 for q = 1:201
@@ -39,3 +40,50 @@ for q = 1:201
    k = k+.1;
 end
 plot(x,fx)
+
+close all;
+clear;
+
+%ex 2 - Fibonacci
+
+n = input('N interacoes:');
+a0 = 1;
+a1 = a0;
+compara1 = (1+sqrt(5))/2;
+
+for ite = 1:n
+    aux = a0 + a1;
+    a0 = a1;
+    a1 = aux;
+end
+compara2 = a1/a0;
+disp(compara1);
+disp(compara2);
+
+%ex 3 -  Ruido
+%fazendo sinal w
+t = 0:0.0001:1000;
+n = length(t);
+for m = 1:n
+    w(m) = randn();
+end
+%funcao de tranferencia
+H = tf([1 100],[1 1]);
+
+y = lsim(H,w,t);
+
+plot(t,y)
+figure
+fw = fft(w);
+fy = fft(y);
+plot(t,fw);
+hold on
+plot(t,fy);
+hold off
+figure
+bode(H);
+
+
+
+
+
